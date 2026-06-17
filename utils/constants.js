7fa72427747439da94f5fc5fbe3b08afd9d9fe2a
surya <varma.surya@protonmail.com>
2026-06-17 19:01:09 -0400

    chore: update Swiggy API proxy endpoints to use allorigins.win

--- utils/constants.js
+++ utils/constants.js
@@ -2,7 +2,7 @@
 //   data.success.cards[1].gridWidget.gridElements.infoWithStyle.restaurants;
 
 export const SWIGGY_URL =
-  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.6868159&lng=83.2184815";
+  "https://api.allorigins.win/raw?url=https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D17.6868159%26lng%3D83.2184815";
 
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
@@ -11,7 +11,7 @@ export const LOGO_URL =
   "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
 
 export const SWIGGY_MENU_URL =
-  "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.6868159&lng=83.2184815&restaurantId=";
+  "https://api.allorigins.win/raw?url=https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D17.6868159%26lng%3D83.2184815%26restaurantId%3D";
 
 //https://www.swiggy.com/mapi/homepage/getCards?lat=17.6868159&lng=83.2184815
 
